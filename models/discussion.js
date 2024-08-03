37535ffecf599d2cf2a7bcedfcfc19f94c1c0bed
David <aldavid342@gmail.com>
2024-08-03 18:09:25 +0200

    fix indentation

--- models/discussion.js
+++ models/discussion.js
@@ -4,14 +4,13 @@ const discussionSchema = mongoose.Schema({
   user_1: id,
   user_2: id,
   message: messageSchema,
-  
 });
 
 const messageSchema = mongoose.Schema({
-    author: String,
-    message: String,
-    date: date,
-   });
+  author: String,
+  message: String,
+  date: date,
+});
 
 const Discussion = mongoose.model('Discussions', discussionSchema);
 
