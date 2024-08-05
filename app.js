edc772b18263263db6977a5c32f6c0e2a5af9fe2
Aurelien <a.sanctorum1301@gmail.com>
2024-08-05 12:05:13 +0200

    onBoardingBackend

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var on_boardingRouter = require('.')
 
 var app = express();
 const cors = require('cors');
