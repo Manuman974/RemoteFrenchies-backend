312a29817a6ce03dae1c2aa69d07944a8ed0bf5b
Manu <manuelmanrique6@hotmail.com>
2024-08-07 17:17:10 +0200

    merge

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var onboardingRouter = require('./routes/onBoarding');
+var propositionRouter = require('./routes/propositions')
 
 var app = express();
 const cors = require('cors');
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/onboarding', onboardingRouter );
+app.use('/', propositionRouter);
 
 
 module.exports = app;
