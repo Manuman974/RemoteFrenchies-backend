6c62fb511944d558424361cd333e7a3a2e607087
Aurelien <a.sanctorum1301@gmail.com>
2024-08-08 14:10:24 +0200

    route proposer+ route cloudinary+ package.json

--- routes/propositions.js
+++ routes/propositions.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 const User = require('../models/users');
 const Proposition = require('../models/proposition');
+const uniqid = require('uniqid');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 
 router.post('/proposition', (req, res) => {
     // Destructuration du code
@@ -15,59 +18,89 @@ router.post('/proposition', (req, res) => {
         dedicated_office,
         other,
         description,
-        userId
+        token
     } = req.body;
+//user.find req.body.token
+User.findOne({ token }).then(user => {
+    if ( !user) {
+        return res.json({ result: false, error: 'User not found' });
+      
+    }
+});
+// Vérifier si une proposition avec les mêmes détails existe déjà pour l'utilisateur
+Proposition.findOne({
+    main_address: { street: req.body.main_address},
+    welcome_day,
+    reception_hours,
+    fiber_connection,
+    coffee_tea,
+    dedicated_office,
+    other,
+    description,
+    user: User._id
+})
+.then(existingProposition => {
+    console.log(req.body)
+    if (existingProposition) {
+        // erreur si proposition existante
+        res.json({ result: false, error: 'Proposition already exists' });
+    } else {
+        // Creation nouvelle proposition
+        const newProposition = new Proposition({
+            user: User._id,
+            main_address: { street: main_address },
+            welcome_day,
+            reception_hours,
+            fiber_connection,
+            coffee_tea,
+            dedicated_office,
+            other,
+            description
+        });
 
-    
-    // Vérifier si une proposition avec les mêmes détails existe déjà pour l'utilisateur
-    Proposition.findOne({
-        main_address: { street: req.body.main_address},
-        welcome_day,
-        reception_hours,
-        fiber_connection,
-        coffee_tea,
-        dedicated_office,
-        other,
-        description,
-        user: userId
-    })
-    .then(existingProposition => {
-        console.log(req.body)
-        if (existingProposition) {
-            // erreur si proposition existante
-            res.json({ result: false, error: 'Proposition already exists' });
-        } else {
-            // Creation nouvelle proposition
-            const newProposition = new Proposition({
-                user: userId,
-                main_address: { street: main_address },
-                welcome_day,
-                reception_hours,
-                fiber_connection,
-                coffee_tea,
-                dedicated_office,
-                other,
-                description
-            });
+        // Save nouvelle proposition
+        newProposition.save()
+            .then(savedProposition => {
+                // Mettre à jour le champ de proposition de l'utilisateur avec l'ID de la nouvelle proposition
+                User.findByIdAndUpdate(User.id,  { propositions: savedProposition._id } )
+                    .then(() => {
+                        // nouvelle proposition saved
+                        res.json({ result: true, proposition: savedProposition });
+                    })
+                    
+            })
+            
+    }
+})
 
-            // Save nouvelle proposition
-            newProposition.save()
-                .then(savedProposition => {
-                    // Mettre à jour le champ de proposition de l'utilisateur avec l'ID de la nouvelle proposition
-                    User.findByIdAndUpdate(userId, { $push: { propositions: savedProposition._id } })
-                        .then(() => {
-                            // nouvelle proposition saved
-                            res.json({ result: true, proposition: savedProposition });
-                        })
-                        
-                })
-                
-        }
-    })
-    
 });
 
+
+
+router.post('/upload', async (req, res) => {
+const photoPath = `./tmp/${uniqid()}.jpg`;
+const resultMove = await req.files.photoFromFront.mv(photoPath);
+
+if (!resultMove) {
+    const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+    fs.unlinkSync(photoPath);
+  res.json({ result: true, url: resultCloudinary.secure_url });      
+} else {
+  res.json({ result: false, error: resultMove });
+}
+
+
+});
+
+
 module.exports = router;
+
+    
+
+    
+
+
+
 
 
         
