be063554962f44d78acc266f902262eda4a4a8b5
Manu <manuelmanrique6@hotmail.com>
2024-09-10 14:33:33 +0200

    config vercel2

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
-const express = require("express");
-const app = express();
+var express = require('express');
+var router = express.Router();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
-
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// module.exports = router;
+module.exports = router;
